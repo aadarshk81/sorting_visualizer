bc1bd7aedac0e36a6be7f653f8847902a01b5544
aadarshk81 <aadarshk81@gmail.com>
2021-06-22 10:13:59 +0530

    src commit

---
+++ src/Algos/main.js
@@ -0,0 +1,40 @@
+const funcs = {
+//for swapping two elements
+ swap(el1, el2) {
+    
+    let temp = el1.style.height;
+    el1.style.height = el2.style.height;
+    el2.style.height = temp;
+    
+},
+
+//wait 
+ waitforme(milisec) { 
+    return new Promise(resolve => { 
+        setTimeout(() => { resolve('') }, milisec); 
+    }) 
+},
+
+ disable() {
+    document.getElementById("new-array").disabled=true;
+    
+
+    document.getElementById("arr_sz").disabled=true;
+    
+
+    document.getElementById("speed_input").disabled=true;
+    
+},
+
+ enable() {
+     document.getElementById("new-array").disabled=false;
+    
+
+     document.getElementById("arr_sz").disabled=false;
+    
+
+     document.getElementById("speed_input").disabled=false;
+    
+}
+}
+export default funcs;
